95d5a923f7c7e405a9ef9cf489b08dcc1cee5975
Valeriy Klimchenko <klimmail.ru@mail.ru>
2023-04-05 21:41:07 +0300

    layout

--- Netology_IB_Instruments2/Netology_IB_Instruments2/ProfileViewController.swift
+++ Netology_IB_Instruments2/Netology_IB_Instruments2/ProfileViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    private var myView: ProfileView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView
+        
+        view.addSubview(myView)
     }
+    
+    
+    
 }
 
 
