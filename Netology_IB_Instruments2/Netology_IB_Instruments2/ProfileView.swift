0c03d053a940ae54772bd921df7a3c00836c5e88
Valeriy Klimchenko <klimmail.ru@mail.ru>
2023-04-07 00:08:07 +0300

    add @IBOutlet

--- Netology_IB_Instruments2/Netology_IB_Instruments2/ProfileView.swift
+++ Netology_IB_Instruments2/Netology_IB_Instruments2/ProfileView.swift
@@ -9,4 +9,9 @@ import UIKit
 
 class ProfileView: UIView {
     
+    @IBOutlet weak var foto: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var dateOfBirth: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var myText: UITextView!
 }
